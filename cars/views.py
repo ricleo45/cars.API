e212350d1db352bc14f30f343796b84f42a7bede
ricardo <ricleo45@yahoo.com>
2022-06-07 00:11:10 -0600

    completed cars.API tutorial

---
+++ cars/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import get_object_or_404
+from os import stat
+from django.urls import is_valid_path
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import CarSerializer
+from .models import Car
+from rest_framework import status
+
+
+@api_view(['GET', 'POST'])
+def cars_list(request):
+    
+    if request.method == 'GET':
+        cars = Car.objects.all()
+        serializers = CarSerializer(cars, many=True)
+        return Response(serializers.data)
+    
+    elif request.method == 'POST':
+        serializer = CarSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def car_detail(request, pk):
+    car = get_object_or_404(Car, pk=pk)
+    if request.method == 'GET':
+        serializer = CarSerializer(car)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = CarSerializer(car, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        car.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
---
+++ cars_project/local_setting.py
@@ -0,0 +1,16 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-yay)#@#cajt_9%tvpx^lp)mkm0nt%f=3ja75evhkrzv##d@m-e'
+
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cars_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '96b2110e3D'
+    }
+}
